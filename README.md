8a4ab0c6598ad34d277e23a7e254784e51a592ae
fox-yokai <pat.w.cain@gmail.com>
2020-03-26 22:38:56 -0500

    somewhat working app

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # README-generator
+The purpose of this is to provide an easy to use README.md generator
+## Usage
+```
+node index.js
+```
+## Areas to Improve
+The following areas still need to function
+- ability to create a Table of Content
+- ability to decline to add installation instructions
+- ability to add badges
--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// The README will be populated with the following:
-
-// At least one badge
-// Project title
-// Description
-// Table of Contents
-// Installation
-// Usage
-// License
-// Contributing
-// Tests
-// Questions
-
-// User GitHub profile picture
-// User GitHub email
-
 const util = require("util");
 const fs = require("fs");
 const inquirer = require("inquirer");
@@ -25,72 +9,96 @@ const readFileAsync = util.promisify(fs.readFile);
 
 // prompt the user for title, description, table of contents, installation, usage, license, contributing, tests, questions
 inquirer
-  .prompt([
+    .prompt([
         {
-            type: "input",
-            message: "What is your GitHub username?",
-            name: "username",
-            // should validate here
-        },
-        {
-            type: "input",
-            message: "What is the title of your project?",
-            name: "projectTitle",
-            // should validate that its not null or uses spaces
-        },
-        {
-            type: "input",
-            message: "Enter a description",
-            name: "description",
-            // should validate that its not blank
-        },
-        {
-            type: "confirm",
-            message: "Do you wish to create a Table of Contents now?",
-            name: "confimToC",
-        },
-        {
-            type: "input",
-            message: "What are the installation instructions?",
-            name: "installation",
-        },
-        {
-            type: "input",
-            message: "Provide instructions and examples for use",
-            name: "usage",
-        },
-        {
-            type: "list",
-            message: "Select license type",
-            choices: ["none", "MIT", "GPLv2", "Apache"],
-            name: "license",
-        },
-        {
-            type: "input",
-            message: "If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so.",
-            name: "contributing",
-        },
-        {
-            type: "input",
-            message: "Provide any examples on how to run your application here",
-            name: "tests",
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username",
+        // add validation here
+    },
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "projectTitle",
+        // should validate that its not null or uses spaces
+    },
+    {
+        type: "input",
+        message: "Enter a description",
+        name: "description",
+        // should validate that its not blank
+    },
+    {
+        type: "confirm",
+        message: "Do you wish to create a Table of Contents now?",
+        name: "confirmToC",
+    },
+    {
+        type: "confirm",
+        message: "Do you wish to add installation instructions now?",
+        name: "confirmInstall",
+    },
+    {
+        type: "input",
+        message: "What are the installation instructions?",
+        name: "installation",
+        when: function (answers) {
+            return answers.confirmInstall === true;
         }
-  ])
-  .then(answers => {
-      // use axios to contact the GitHub API
-        // retrive the user's email and profile image
-        const queryUrl = `https://api.github.com/users/${answers.username}`;
+    },
+    {
+        type: "input",
+        message: "Provide instructions and examples for use",
+        name: "usage",
+    },
+    {
+        type: "list",
+        message: "Select license type",
+        choices: ["none", "MIT", "GPLv2", "Apache"],
+        name: "license",
+    },
+    {
+        type: "input",
+        message: "If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so.",
+        name: "contributing",
+    },
+    {
+        type: "input",
+        message: "Provide any examples on how to run your application here",
+        name: "tests",
+    }
+])
+    .then(answers => {
+    // take the input above and append to READNE.md file
+    // append title to README.md
 
-        axios
-        .get(queryUrl)
-        .then(function(res) {
-            const gitImageUrl = res.data.avatar_url;
-            const gitEmail = res.data.email;
+    // its late and I'm just trying to finish something to hand in at this point
 
-    // take the input above and create a READNE.md file
+        let title = answers.projectTitle;
+        let description = answers.description;
+        let install = answers.installation;
+        let use = answers.usage;
+        let lic = answers.license;
+        let contrib = answers.contributing;
+        let test = answers.tests;
 
-    // dynamically create Table of Contents
-    // append the data above to file
+        appendFileAsync("README.md",`# ${title}\n${description}\n## Installation\n${install}\n## Usage\n${use}\n## License\n${lic}\n## Contributing\n${contrib}\n## Tests\n${test}`);
+
+
+    
+    // use axios to contact the GitHub API
+      // retrive the user's email and profile image
+      let queryUrl = `https://api.github.com/users/${answers.username}`;
+
+      axios
+      .get(queryUrl)
+      .then(function(res) {
+          // need to add error handling for null values
+        const gitEmail = res.data.email;
+        const gitImg =  res.data.avatar_url;
+
+        return appendFileAsync("README-test.md",`\n## Authors\n- ${answers.username} -_Initial work_- ${gitEmail} ![the author](${gitImg})`);
+      })
   })
   .catch(error => {
     if(error.isTtyError) {
@@ -98,4 +106,4 @@ inquirer
     } else {
       // Something else when wrong
     }
-  });
+  })
